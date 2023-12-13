eb6e7e18f1f9e61887a4b848123f6ab1ae9efd67
Bruce Rodriguez <realbrucest@yahoo.es>
2023-12-13 22:59:18 +0100

    Slider and test copper button added to GUI

--- hspaint.py
+++ hspaint.py
@@ -1,18 +1,16 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QHBoxLayout, QColorDialog, QWidget, QFileDialog, QRadioButton, QGroupBox, QShortcut, QScrollArea
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QHBoxLayout, QColorDialog, QWidget, QFileDialog, QRadioButton, QGroupBox, QShortcut, QScrollArea, QCheckBox, QPushButton, QSlider
 from PyQt5.QtGui import QPixmap, QImage, QColor, QKeySequence
 from PyQt5.QtCore import Qt, QEvent
 from PIL import Image
+from random import randint
 import sys
 
-
 DEFAULT_ZOOM_LEVEL = 4.0
-
 
 class ImageLine:
     def __init__(self, image, palette_index):
         self.image = image
         self.palette_index = palette_index
-
 
 class ImageEditorWidget(QWidget):
     def __init__(self, image_path):
@@ -64,6 +62,21 @@ class ImageEditorWidget(QWidget):
             self.zoom_radio_buttons.append(radio_button)
             zoom_layout.addWidget(radio_button)
 
+        # Nuevo control para activar/desactivar el efecto Copper
+        self.copper_checkbox = QCheckBox("Activar Copper", self)
+        self.copper_checkbox.setChecked(False)
+        self.copper_checkbox.stateChanged.connect(self.toggle_copper_effect)
+
+        # Nuevo botón para generar una nueva paleta
+        self.generate_palette_button = QPushButton("Generar Nueva Paleta", self)
+        self.generate_palette_button.clicked.connect(self.generate_new_palette)
+
+        # Nuevo control deslizante para seleccionar la posición del Copper
+        self.copper_position_slider = QSlider(Qt.Horizontal, self)
+        self.copper_position_slider.setMaximum(self.image.height - 1)
+        self.copper_position_slider.setValue(0)
+        self.copper_position_slider.valueChanged.connect(self.update_copper_position)
+
         # Establecer el zoom predeterminado
         default_zoom_index = zoom_levels.index(DEFAULT_ZOOM_LEVEL)
         self.zoom_radio_buttons[default_zoom_index].setChecked(True)
@@ -93,6 +106,8 @@ class ImageEditorWidget(QWidget):
         self.y_position_label = QLabel(self)
         side_layout.addWidget(self.y_position_label)
 
+        # ... (continuación del código)
+
         main_layout_splitter.addWidget(side_panel)
         main_layout.addLayout(main_layout_splitter)
 
@@ -100,44 +115,80 @@ class ImageEditorWidget(QWidget):
         main_layout.addWidget(color_group_box)
         main_layout.addWidget(zoom_group_box)
 
+        # Agregar controles para el efecto Copper
+        main_layout.addWidget(self.copper_checkbox)
+        main_layout.addWidget(self.generate_palette_button)
+        main_layout.addWidget(self.copper_position_slider)
+
         # Conectar el evento de movimiento del ratón para actualizar la posición Y
         self.scroll_area.installEventFilter(self)
 
     def initialize_image_lines(self):
-        # Crear una línea para cada fila de la imagen original
-        for y in range(self.image.height):
-            line_image = self.image.crop((0, y, self.image.width, y + 1))
-            palette_index = 0  # Inicializar con la paleta original
-            image_line = ImageLine(line_image, palette_index)
-            self.image_lines.append(image_line)
+        # Inicializar las líneas de la imagen
+        for i in range(self.image.height):
+            line_image = self.image.crop((0, i, self.image.width, i + 1))
+            palette_index = i % 16  # Inicializar con el índice correspondiente a la paleta original
+            self.image_lines.append(ImageLine(line_image, palette_index))
 
+    def toggle_copper_effect(self, state):
+        if state == Qt.Checked:
+            self.apply_copper_effect()
+        else:
+            self.reset_copper_effect()
+    
     def convert_pil_to_qimage(self, pil_image):
         image = pil_image.convert("RGBA")
         width, height = image.size
         bytes_per_line = 4 * width
         q_image = QImage(image.tobytes("raw", "RGBA"), width, height, bytes_per_line, QImage.Format_RGBA8888)
         return q_image
+    
+    def set_zoom_level(self, level):
+        # Guardar el estado actual en el historial
+        self.edit_history.append(self.get_current_state())
 
-    def get_palette_color(self, index):
-        # Obtener la paleta actual
-        palette = self.image.getpalette()
+        new_size = (self.image.width, int(self.image.height * level))
+        for image_line in self.image_lines:
+            image_line.image = image_line.image.resize(new_size)
 
-        # Obtener el color de la paleta correspondiente al índice
-        color = palette[index * 3: (index + 1) * 3]
-        return tuple(color)
+        # Actualizar la etiqueta de la imagen con el nivel de zoom actual
+        self.update_image_with_current_zoom()
 
-    def get_combined_image(self):
+    def get_current_state(self):
+        return {
+            'palettes': {y: image_line.palette_index for y, image_line in enumerate(self.image_lines)},
+            'zoom_level': self.get_current_zoom_level(),
+        }
+    
+    def get_current_zoom_level(self):
+        for i, radio_button in enumerate(self.zoom_radio_buttons):
+            if radio_button.isChecked():
+                return float(radio_button.text().replace('x', ''))
+            
+    def update_image_with_current_zoom(self):
+        current_zoom = self.get_current_zoom_level()
+
         # Crear una nueva imagen con todas las líneas combinadas
-        combined_image = Image.new("RGBA", (self.image.width, self.image.height))
+        combined_image = self.get_combined_image()
 
-        for y, image_line in enumerate(self.image_lines):
-            combined_image.paste(image_line.image, (0, y))
+        # Escalar la imagen de acuerdo al nivel de zoom
+        new_size = (int(combined_image.width * current_zoom), int(combined_image.height * current_zoom))
+        resized_image = combined_image.resize(new_size)
 
-        return combined_image
+        # Mostrar la imagen combinada y escalada en la etiqueta
+        self.image_label.setPixmap(QPixmap.fromImage(self.convert_pil_to_qimage(resized_image)))
 
-    def update_color_label(self, index):
-        color = self.get_palette_color(index)
-        self.color_label_group[index].setStyleSheet(f"background-color: rgb{color};")
+    def undo_last_change(self):
+        # Deshacer el último cambio si hay cambios en el historial
+        if self.edit_history:
+            last_state = self.edit_history.pop()
+
+            # Restaurar la paleta y el nivel de zoom
+            for y, palette_index in last_state['palettes'].items():
+                self.image_lines[y].palette_index = palette_index
+
+            # Actualizar la etiqueta de la imagen con el nivel de zoom actual
+            self.update_image_with_current_zoom()
 
     def show_color_picker(self, event, index):
         # Abrir el diálogo de selección de color
@@ -166,52 +217,70 @@ class ImageEditorWidget(QWidget):
             palette[index * 3: (index + 1) * 3] = new_color
             image_line.image.putpalette(palette)
 
-    def set_zoom_level(self, level):
+    def update_color_label(self, index):
+        color = self.get_palette_color(index)
+        self.color_label_group[index].setStyleSheet(f"background-color: rgb{color};")
+
+
+    def get_combined_image(self):
+        # Crear una nueva imagen con todas las líneas combinadas
+        combined_image = Image.new("RGBA", (self.image.width, self.image.height))
+
+        for y, image_line in enumerate(self.image_lines):
+            combined_image.paste(image_line.image, (0, y))
+
+        return combined_image
+    
+    def get_palette_color(self, index):
+        # Obtener la paleta actual
+        palette = self.image.getpalette()
+
+        # Obtener el color de la paleta correspondiente al índice
+        color = palette[index * 3: (index + 1) * 3]
+        return tuple(color)
+
+    def apply_copper_effect(self):
         # Guardar el estado actual en el historial
         self.edit_history.append(self.get_current_state())
 
-        new_size = (self.image.width, int(self.image.height * level))
-        for image_line in self.image_lines:
-            image_line.image = image_line.image.resize(new_size)
+        # Aplicar el efecto Copper cambiando las paletas de las líneas de la imagen
+        for i, image_line in enumerate(self.image_lines):
+            image_line.palette_index = i % 16
 
         # Actualizar la etiqueta de la imagen con el nivel de zoom actual
         self.update_image_with_current_zoom()
 
-    def get_current_zoom_level(self):
-        for i, radio_button in enumerate(self.zoom_radio_buttons):
-            if radio_button.isChecked():
-                return float(radio_button.text().replace('x', ''))
+    def reset_copper_effect(self):
+        # Restaurar el estado del efecto Copper
+        self.undo_last_change()
 
-    def undo_last_change(self):
-        # Deshacer el último cambio si hay cambios en el historial
-        if self.edit_history:
-            last_state = self.edit_history.pop()
+    def generate_new_palette(self):
+        # Generar una nueva paleta de colores
+        new_palette = [(randint(0, 255), randint(0, 255), randint(0, 255)) for _ in range(16)]
 
-            # Restaurar la paleta y el nivel de zoom
-            for y, palette_index in last_state['palettes'].items():
-                self.image_lines[y].palette_index = palette_index
+        # Guardar el estado actual en el historial
+        self.edit_history.append(self.get_current_state())
 
-            # Actualizar la etiqueta de la imagen con el nivel de zoom actual
-            self.update_image_with_current_zoom()
+        # Cambiar los colores en la paleta
+        for i in range(16):
+            self.change_palette_color_at_index(i % 16, new_palette[i])
 
-    def update_image_with_current_zoom(self):
-        current_zoom = self.get_current_zoom_level()
+            # Actualizar el color del label
+            self.update_color_label(i % 16)
 
-        # Crear una nueva imagen con todas las líneas combinadas
-        combined_image = self.get_combined_image()
+        # Actualizar la etiqueta de la imagen con el nivel de zoom actual
+        self.update_image_with_current_zoom()
 
-        # Escalar la imagen de acuerdo al nivel de zoom
-        new_size = (int(combined_image.width * current_zoom), int(combined_image.height * current_zoom))
-        resized_image = combined_image.resize(new_size)
+    def update_copper_position(self, position):
+        # Guardar el estado actual en el historial
+        self.edit_history.append(self.get_current_state())
 
-        # Mostrar la imagen combinada y escalada en la etiqueta
-        self.image_label.setPixmap(QPixmap.fromImage(self.convert_pil_to_qimage(resized_image)))
+        # Desplazar el efecto Copper cambiando las paletas de las líneas de la imagen
+        for i, image_line in enumerate(self.image_lines):
+            image_line.palette_index = (i + position) % 16
 
-    def get_current_state(self):
-        return {
-            'palettes': {y: image_line.palette_index for y, image_line in enumerate(self.image_lines)},
-            'zoom_level': self.get_current_zoom_level(),
-        }
+        # Actualizar la etiqueta de la imagen con el nivel de zoom actual
+        self.update_image_with_current_zoom()
 
     def update_mouse_position(self, event):
         # Obtener la posición X e Y del ratón
@@ -222,6 +291,11 @@ class ImageEditorWidget(QWidget):
         self.x_position_label.setText(f"Posición X: {x_position}")
         self.y_position_label.setText(f"Posición Y: {y_position}")
 
+    def eventFilter(self, watched, event):
+        # Filtrar eventos, en este caso, eventos de movimiento del ratón en el área de desplazamiento
+        if watched == self.scroll_area and event.type() == QEvent.MouseMove:
+            self.update_mouse_position(event)
+        return super().eventFilter(watched, event)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
