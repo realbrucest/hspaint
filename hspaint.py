4714bf3ebda1f7db1529dc8a37459122bde8da0c
Bruce Rodriguez <realbrucest@yahoo.es>
2023-12-13 12:19:16 +0100

    Added QScrollArea to place the image

--- hspaint.py
+++ hspaint.py
@@ -1,5 +1,5 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QHBoxLayout, QColorDialog, QWidget, QFileDialog, QRadioButton, QGroupBox
-from PyQt5.QtGui import QPixmap, QImage, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QHBoxLayout, QColorDialog, QWidget, QFileDialog, QRadioButton, QGroupBox, QShortcut, QScrollArea
+from PyQt5.QtGui import QPixmap, QImage, QColor, QKeySequence
 from PIL import Image
 import sys
 
@@ -11,19 +11,30 @@ class ImageEditorWidget(QWidget):
         self.image_path = image_path
         self.image = Image.open(image_path)
 
+        # Historial de paletas y niveles de zoom para deshacer
+        self.edit_history = []
+
         # Crear una etiqueta para mostrar la imagen
         self.image_label = QLabel(self)
         self.image_label.setPixmap(QPixmap.fromImage(self.convert_pil_to_qimage(self.image)))
+
+        # Colocar la etiqueta dentro de un QScrollArea
+        scroll_area = QScrollArea(self)
+        scroll_area.setWidgetResizable(True)
+        scroll_area.setWidget(self.image_label)
 
         # Grupo de labels para la paleta de 16 colores
         color_group_box = QGroupBox("Paleta de Colores")
         color_layout = QHBoxLayout(color_group_box)
         color_layout.setContentsMargins(0, 0, 0, 0)
         self.color_label_group = []
+        self.original_colors = []  # Almacena los colores originales de los color-pickers
         for i in range(16):
             color_label = QLabel(self)
             color_label.setFixedSize(30, 30)
-            color_label.setStyleSheet(f"background-color: rgb{self.get_palette_color(i)};")
+            original_color = self.get_palette_color(i)
+            self.original_colors.append(original_color)
+            color_label.setStyleSheet(f"background-color: rgb{original_color};")
             color_label.mousePressEvent = lambda event, i=i: self.show_color_picker(event, i)
             self.color_label_group.append(color_label)
             color_layout.addWidget(color_label)
@@ -33,16 +44,24 @@ class ImageEditorWidget(QWidget):
         zoom_layout = QHBoxLayout(zoom_group_box)
         zoom_layout.setContentsMargins(0, 0, 0, 0)
         self.zoom_radio_buttons = []
-        zoom_levels = [1.0, 1.5, 2.0, 3.0, 4.0]
+        zoom_levels = [1.0, 1.5, 2.0, 3.0, 4.0, 5.0, 6.0]
         for level in zoom_levels:
             radio_button = QRadioButton(f"{level}x", self)
             radio_button.clicked.connect(lambda _, level=level: self.set_zoom_level(level))
             self.zoom_radio_buttons.append(radio_button)
             zoom_layout.addWidget(radio_button)
 
+        # Establecer 4x como el zoom inicial
+        self.zoom_radio_buttons[4].setChecked(True)  # Índice 4 corresponde a 4.0 en la lista de niveles de zoom
+        self.set_zoom_level(4.0)
+
+        # Atajo de teclado para deshacer (Ctrl+Z)
+        undo_shortcut = QShortcut(QKeySequence("Ctrl+Z"), self)
+        undo_shortcut.activated.connect(self.undo_last_change)
+
         # Diseño del diseño principal
         layout = QVBoxLayout(self)
-        layout.addWidget(self.image_label)
+        layout.addWidget(scroll_area)  # Usar QScrollArea en lugar de la etiqueta directamente
         layout.addWidget(color_group_box)
         layout.addWidget(zoom_group_box)
 
@@ -73,6 +92,13 @@ class ImageEditorWidget(QWidget):
         self.color_label_group[index].setStyleSheet(f"background-color: rgb{color};")
 
     def show_color_picker(self, event, index):
+        # Guardar la paleta y el nivel de zoom actual en el historial
+        current_state = {
+            'palette': self.image.getpalette().copy(),
+            'zoom_level': self.get_current_zoom_level(),
+        }
+        self.edit_history.append(current_state)
+
         # Abrir el diálogo de selección de color
         color = QColorDialog.getColor(QColor(*self.get_palette_color(index)), self)
 
@@ -86,8 +112,8 @@ class ImageEditorWidget(QWidget):
             # Actualizar el color del label
             self.update_color_label(index)
 
-            # Actualizar la etiqueta de la imagen
-            self.image_label.setPixmap(QPixmap.fromImage(self.convert_pil_to_qimage(self.image)))
+            # Actualizar la etiqueta de la imagen con el nivel de zoom actual
+            self.update_image_with_current_zoom()
 
     def change_palette_color_at_index(self, index, new_color):
         # Obtener la paleta actual
@@ -100,7 +126,40 @@ class ImageEditorWidget(QWidget):
         self.image.putpalette(palette)
 
     def set_zoom_level(self, level):
+        # Guardar la paleta y el nivel de zoom actual en el historial
+        current_state = {
+            'palette': self.image.getpalette().copy(),
+            'zoom_level': self.get_current_zoom_level(),
+        }
+        self.edit_history.append(current_state)
+
         new_size = (int(self.image.width * level), int(self.image.height * level))
+        resized_image = self.image.resize(new_size)
+        self.image_label.setPixmap(QPixmap.fromImage(self.convert_pil_to_qimage(resized_image)))
+
+    def get_current_zoom_level(self):
+        for i, radio_button in enumerate(self.zoom_radio_buttons):
+            if radio_button.isChecked():
+                return float(radio_button.text().replace('x', ''))
+
+    def undo_last_change(self):
+        # Deshacer el último cambio si hay cambios en el historial
+        if self.edit_history:
+            last_state = self.edit_history.pop()
+
+            # Restaurar la paleta y el nivel de zoom
+            self.image.putpalette(last_state['palette'])
+            self.set_zoom_level(last_state['zoom_level'])
+
+            # Revertir los colores de los color-pickers
+            for i, color in enumerate(last_state['palette'][:48:3]):  # Tomar solo los valores R de la paleta
+                self.color_label_group[i].setStyleSheet(f"background-color: rgb{self.original_colors[i]};")
+
+            self.update_image_with_current_zoom()
+
+    def update_image_with_current_zoom(self):
+        current_zoom = self.get_current_zoom_level()
+        new_size = (int(self.image.width * current_zoom), int(self.image.height * current_zoom))
         resized_image = self.image.resize(new_size)
         self.image_label.setPixmap(QPixmap.fromImage(self.convert_pil_to_qimage(resized_image)))
 
@@ -114,13 +173,13 @@ if __name__ == '__main__':
     if not image_path:
         sys.exit()
 
-    # Crea y muestra la ventana principal de la aplicación
+    # Crea y muestra la ventana principal de la aplicación maximizada
     window = QMainWindow()
     window.setGeometry(100, 100, 800, 600)
     window.setWindowTitle("Editor de Imágenes")
+    window.showMaximized()  # Mostrar maximizado
     
     image_editor = ImageEditorWidget(image_path)
     window.setCentralWidget(image_editor)
 
-    window.show()
     sys.exit(app.exec_())
